4565719daf35f7f6e27f13f693e5c90cfff25567
yeseullynlee <lysl1025@gmail.com>
2023-07-20 00:46:16 -0500

    coded second solution.

--- PalindromeNumber.js
+++ PalindromeNumber.js
@@ -5,6 +5,8 @@
  * @param {number} x
  * @return {boolean}
  */
+
+//First solution
 var isPalindrome = function(x) {
 
     //y를 선언하고 x값을 y에 넣는다.
@@ -25,6 +27,36 @@ var isPalindrome = function(x) {
         return false;
     }
     return true;
+};
+
+//Second solution
+var isPalindrome = function (x) {
+
+    if (x < 0) {
+        return false;
+    }
+    // Result는 나머지 값을 저장할 숫자 에레이.
+    let result = [];
+
+    // y에 x 초기값을 저장해준다.
+    let y = x;
+
+    // for문으로 x를 순회한다.
+    for (let i = 0; i < y.toString().length; i++) {
+        // x(121)를 %10 나누고 나머지 1을 result에 넣는다.
+        result[i] = Math.floor(x % 10);
+        // x를 10 으로 나누고 값을 다시 x에 저장한다.
+        x = Math.floor(x / 10);
+    }
+        
+    // for문으로 result를 순회한다.
+    for (let i = 0; i < result.length; i++)
+    {
+        // x에 result[i] 곱하기 10의 result.length -1 -i 값을 더해준다.
+        x += result[i] * 10 ** (result.length -1 -i);
+        }
+    // y와 x 값이 같으면 true, 다르면 false를 리턴한다.
+    return y === x;
 };
 
 console.log(isPalindrome(121));
