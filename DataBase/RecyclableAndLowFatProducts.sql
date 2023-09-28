a603ff60da8a32c63a7184dfbb22bf6860c6aec6
yeseullynlee <lysl1025@gmail.com>
2023-09-27 19:27:45 -0500

    A solution to find the ids of products that are both low fat and recyclabl.

---
+++ DataBase/RecyclableAndLowFatProducts.sql
@@ -0,0 +1,8 @@
+-- Write a solution to find the ids of products that are both low fat and recyclable.
+-- Return the result table in any order.
+-- The result format is in the following example.
+
+-- # Write your MySQL query statement below
+SELECT product_id
+FROM Products
+WHERE low_fats = 'Y' AND recyclable = 'Y'
